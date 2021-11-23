4d97a81ab060fe845d4d2e3a46f7291159a1817f
collins <collinsboniface1000@gmail.com>
2021-11-23 10:22:47 +0200

    Creating a new typeConversion file to show how to convert the data type from one type to another type

---
+++ typeConversion.dart
@@ -0,0 +1,13 @@
+
+main (){
+  //String -> int
+  var one = int.parse('1');
+
+  //The build int assert function  to verify that 1 == 1
+  assert(one == 1);
+
+  //String -> double
+  var onePointOne = double.parse('1.1');
+  assert(onePointOne == 1.1);
+
+}
