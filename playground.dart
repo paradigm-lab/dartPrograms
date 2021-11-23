6a32a9d968665ae6ff94fab286da0a00c22bfe7a
collins <collinsboniface1000@gmail.com>
2021-11-23 09:29:28 +0200

    Adding the comment in the file for the explanation of the variable definitions

--- playground.dart
+++ playground.dart
@@ -1,6 +1,11 @@
 
 void main() {
+
+    // Defining the variable using the var, This will tell the compile that this is the string type this is type inference
+    // Dart support both type inference and static type defined
     var firstName = "Collins";
+
+    // We define the variable as the string
     String lastName = "Boniface";
 
     print(firstName + " " + lastName);
