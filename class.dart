89d876ffed46f5726c2fc5cb5b170b61b4fe72e3
collins <collinsboniface1000@gmail.com>
2021-11-23 16:10:41 +0200

    Creating the named constructor apart from the default constructor

--- class.dart
+++ class.dart
@@ -6,18 +6,40 @@ class Person{
 
   //Defining a constructor
   // Constructor is a method that has no return type
+  // Basically the constructor is used to assign the values into the fields or properties of the class.
+  // We can also make the constructor without the body
+  /*
   Person(String name, [int age = 17]) {
       this.name = name;
       this.age = age;
   }
+   */
+
+  Person(this.name, [this.age = 17]);
+
+  // Defining the named constructor
+  // The named constructor is that is not the default constructor of the class
+  Person.guest(){
+    name = "Guest";
+    age = 18;
+  }
+
 
   // The functions inside the class are called the method
   void showOutput() {
-    print("My name is $name and i am $age years old.");
+    print("My name is $name and, I am $age years old.");
   }
 }
 
 void main() {
   Person person1 = Person("Collins");
   person1.showOutput();
+
+  // By using the type inference the compile will now that
+  // person2 is an object of the Person class
+  var person2 = Person("Matt", 32);
+  person2.showOutput();
+
+  var person3 = Person.guest();
+  person3.showOutput();
 }
