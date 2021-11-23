07b5b2e09e37b13bde88dc06bc7f1666974fb2eb
collins <collinsboniface1000@gmail.com>
2021-11-23 10:29:51 +0200

    Converting the integer and double to string in the dart programming language

--- typeConversion.dart
+++ typeConversion.dart
@@ -1,5 +1,9 @@
 
 main (){
+
+  // If we want to convert an integer or a double value from a string your
+  // should have the string with the integer or the double value
+
   //String -> int
   var one = int.parse('1');
 
@@ -10,4 +14,11 @@ main (){
   var onePointOne = double.parse('1.1');
   assert(onePointOne == 1.1);
 
+  // int -> string
+  String oneAsString = 1.toString();
+  assert(oneAsString == '1');
+
+  // double -> String
+  String piAsString = 3.14159.toStringAsFixed(2);
+  assert(piAsString == '3.14');
 }
