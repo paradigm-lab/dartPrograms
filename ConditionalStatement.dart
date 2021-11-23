4a3dfdfe01e9e23b2f82c2fc04f98f996d4eb93c
collins <collinsboniface1000@gmail.com>
2021-11-23 11:59:23 +0200

    Creating the condition flow using the if, else if and the else statement condition and we have also use the switch case statement to create the control flow of the execution of the program

--- ConditionalStatement.dart
+++ ConditionalStatement.dart
@@ -1,6 +1,14 @@
 
 void main() {
-  int number = 100;
+
+  /*
+    We can write the conditional in different ways when we want to check the condition twice then we use the if else statement
+    When we have multiple condition we can use the if else if block to check for the multiple condition in the control statement
+    We can also use the Switch case statement to check for the condition.
+  */
+
+  int number = 91;
+
 
   if (number % 2 == 0) {
     print("Even");
@@ -11,4 +19,21 @@ void main() {
   else {
     print("Confused");
   }
+
+  // We use the break key word to terminate or to get out of the switch case statement
+  number = 0;
+  switch (number) {
+    case 0:
+      print("Even");
+      break;
+    case 1:
+      print("Odd");
+      break;
+    case 2:
+      print("Even");
+      break;
+    default:
+      print("Confused");
+  }
+
 }
