1d2c319739f96d77ea660a1390e53566f0a8672f
collins <collinsboniface1000@gmail.com>
2021-11-23 10:36:28 +0200

    Creating the constantType file that will use to define the constant variable and using the runtimeType method to print the type

---
+++ constantType.dart
@@ -0,0 +1,22 @@
+
+
+main() {
+
+  // When we define the variable as the constant that means
+  // we can't change the variable at all.
+  // We use the const keyword to define the constant variable.
+
+  const aConstNum = 0;  //Integer constant
+  const aConstBool = true; //bool constant
+  const aConstString = "A constant String"; //String constant
+
+
+  print(aConstNum);
+  print(aConstBool);
+  print(aConstString);
+
+  print(aConstNum.runtimeType);
+  print(aConstBool.runtimeType);
+  print(aConstString.runtimeType);
+
+}
