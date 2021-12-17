98e730699d48cdb66b14aa4245aedaa84d27b550
collins <collinsboniface1000@gmail.com>
2021-12-17 22:26:20 +0200

    Creating an enum and creating the enum object in the main method

---
+++ enum.dart
@@ -0,0 +1,10 @@
+// Enum is a special type of class that represent fixed number of constant values.
+
+enum PaymentStatus {
+  notPaid, OTPSent, Paid,
+}
+
+void main() {
+  final status = PaymentStatus.notPaid;
+
+}
