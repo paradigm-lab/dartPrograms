e5c927aa8092ad5d30b4b9714ba73cc6fba4954b
collins <collinsboniface1000@gmail.com>
2021-12-22 11:20:03 +0200

    Creating a new exception handling file and it containes an exception which is the division by zero and we use the tiler operator to avoid the infinity value because to value is converted to a double value

---
+++ exceptionHandling.dart
@@ -0,0 +1,15 @@
+void main() {
+  // Exception is a technical word used for the error
+  // Exception handling is the process of handling the errors.
+
+  // Division by 0
+  var a = 10;
+  var b = 0;
+
+  // We use the tiler operator to avoid the infinity value to be returned
+  var c = a~/b;
+
+  // We get infinity this is because whenever we divide by zero
+  // It automatically converted to a double value.
+  print(c);
+}
