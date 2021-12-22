b5693348d579fc4e8eb7c929f965eec54dc3104e
collins <collinsboniface1000@gmail.com>
2021-12-22 11:22:25 +0200

    Adding the try and catch block to handle the exception.

--- exceptionHandling.dart
+++ exceptionHandling.dart
@@ -2,14 +2,18 @@ void main() {
   // Exception is a technical word used for the error
   // Exception handling is the process of handling the errors.
 
-  // Division by 0
-  var a = 10;
-  var b = 0;
+  try {
+    // Division by 0
+    var a = 10;
+    var b = 0;
 
-  // We use the tiler operator to avoid the infinity value to be returned
-  var c = a~/b;
+    // We use the tiler operator to avoid the infinity value to be returned
+    var c = a ~/ b;
 
-  // We get infinity this is because whenever we divide by zero
-  // It automatically converted to a double value.
-  print(c);
+    // We get infinity this is because whenever we divide by zero
+    // It automatically converted to a double value.
+    print(c);
+  } catch(e) {
+    print("Division by zero is not possible");
+  }
 }
