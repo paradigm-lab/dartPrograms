45b711316a322e95456e68e141f095429d2317b7
collins <collinsboniface1000@gmail.com>
2021-12-22 12:21:47 +0200

    Using the Future keyword and creating a function that returns a String and within the userMsg function we use the fetchUseerData so to avoid getting the instance of the class we need to use the await keyword and to use the await keyword the function needs to be the async function and the function should be a Future type with the String return type and because we are calling the UserMsg in the main method we need to use the await keyword and the main function should also be async so that we can use await keyword

--- Asynchronous.dart
+++ Asynchronous.dart
@@ -1,22 +1,27 @@
 // Asynchronous operation lets you program keep running will waiting for another operation to finish.
 
-Future<void> fetchUserData() {
-  return Future.delayed(Duration(seconds: 2), () => print("Data"));
+Future<String> fetchUserData() {
+  return Future.delayed(Duration(seconds: 2), () => "Data");
 }
-/*
-String userMsg() {
-  var data = fetchUserData();
+
+Future<String> userMsg() async {
+  // Uncompleted future we get an instance
+  // We use the await keyword to wait for the fetchUserData function to complete
+  // So that we can't get the instance of the class
+
+  var data = await fetchUserData();
   return "$data";
 }
 
- */
 
-void main() {
+
+void main() async {
   // Future, async and await
+  // async and await always go together
 
   // Future represent a result of an asynchronous operation and can have
   // Two states only either uncompleted state and completed state
 
-  fetchUserData();
-  print("Present Data");
+  //fetchUserData();
+  print(await userMsg());
 }
