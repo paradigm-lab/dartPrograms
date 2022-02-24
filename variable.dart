63d53d0ede0d747007f81ac4cb99a2335532bcf8
collins <collinsboniface1000@gmail.com>
2022-02-24 05:12:15 +0200

    Created a Bicycle class with the instance of the class

--- variable.dart
+++ variable.dart
@@ -1,4 +1,21 @@
 
+class Bicycle {
+    late int cadence;
+    late int speed;
+    late int gear;
+
+    // Bicycle(this.cadence, this.speed, this.gear);
+
+    Bicycle(int cadence, int speed, int gear) {
+        this.cadence = cadence;
+        this.speed = speed;
+        this.gear = gear;
+    }
+
+    @override
+    String toString() => 'Bicycle{cadence: ${cadence}, speed: ${speed + 10}, gear: ${gear}}';
+
+}
 
 void main() {
   // Note: All data types in dart are objects
@@ -18,4 +35,8 @@ void main() {
 
   // typedef
 
+
+  // Creating an instance of the Bicycle class
+    var bike = new Bicycle(10, 120, 12);
+    print(bike);
 }
