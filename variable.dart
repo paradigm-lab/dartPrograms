b0d80511330ddbac221468fc5a4697a3c664920d
collins <collinsboniface1000@gmail.com>
2022-02-24 04:51:43 +0200

    Added the comment

---
+++ variable.dart
@@ -0,0 +1,21 @@
+
+
+void main() {
+  // Note: All data types in dart are objects
+
+  // Numbers: 1. int
+  //          2. double
+
+
+  // Strings
+
+
+  // Boolean
+
+
+  // Function
+
+
+  // typedef
+
+}
